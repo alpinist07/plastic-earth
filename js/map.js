c4e758f218cba8806c01efa795f115012b873642
Sunjin Lee <alpinist07@naver.com>
2019-01-18 15:41:54 +0900

    검색창으로 맵 호출 테스트

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
-        center: new daum.maps.LatLng(36.90611, 127.03842), // 지도의 중심좌표
-        level: 11, // 지도의 확대 레벨
+        center: new daum.maps.LatLng(37.56665, 126.97917), // 지도의 중심좌표
+        level: 4, // 지도의 확대 레벨
         mapTypeId : daum.maps.MapTypeId.ROADMAP // 지도종류
     };
 
@@ -14,88 +14,63 @@ var mapTypeControl = new daum.maps.MapTypeControl();
 // 지도의 상단 우측에 지도 타입 변경 컨트롤을 추가한다
 map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);
 
-// 지도에 확대 축소 컨트롤을 생성한다
-var zoomControl = new daum.maps.ZoomControl();
-
-// 지도의 우측에 확대 축소 컨트롤을 추가한다
-map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
-
 // 지도 중심 좌표 변화 이벤트를 등록한다
 daum.maps.event.addListener(map, 'center_changed', function () {
-	console.log('지도의 중심 좌표는 ' + map.getCenter().toString() +' 입니다.');
+  console.log('지도의 중심 좌표는 ' + map.getCenter().toString() +' 입니다.');
 });
 
 // 지도 확대 레벨 변화 이벤트를 등록한다
 daum.maps.event.addListener(map, 'zoom_changed', function () {
-	console.log('지도의 현재 확대레벨은 ' + map.getLevel() +'레벨 입니다.');
+  console.log('지도의 현재 확대레벨은 ' + map.getLevel() +'레벨 입니다.');
 });
 
 // 지도 영역 변화 이벤트를 등록한다
 daum.maps.event.addListener(map, 'bounds_changed', function () {
-	var mapBounds = map.getBounds(),
-		message = '지도의 남서쪽, 북동쪽 영역좌표는 ' +
-					mapBounds.toString() + '입니다.';
+  var mapBounds = map.getBounds(),
+    message = '지도의 남서쪽, 북동쪽 영역좌표는 ' +
+          mapBounds.toString() + '입니다.';
 
-	console.log(message);
+  console.log(message);
 });
 
 // 지도 시점 변화 완료 이벤트를 등록한다
 daum.maps.event.addListener(map, 'idle', function () {
-	var message = '지도의 중심좌표는 ' + map.getCenter().toString() + ' 이고,' +
-					'확대 레벨은 ' + map.getLevel() + ' 레벨 입니다.';
-	console.log(message);
+  var message = '지도의 중심좌표는 ' + map.getCenter().toString() + ' 이고,' +
+          '확대 레벨은 ' + map.getLevel() + ' 레벨 입니다.';
+  console.log(message);
 });
 
 // 지도 클릭 이벤트를 등록한다 (좌클릭 : click, 우클릭 : rightclick, 더블클릭 : dblclick)
 daum.maps.event.addListener(map, 'click', function (mouseEvent) {
-	console.log('지도에서 클릭한 위치의 좌표는 ' + mouseEvent.latLng.toString() + ' 입니다.');
+  console.log('지도에서 클릭한 위치의 좌표는 ' + mouseEvent.latLng.toString() + ' 입니다.');
 });
 
 // 지도 드래깅 이벤트를 등록한다 (드래그 시작 : dragstart, 드래그 종료 : dragend)
 daum.maps.event.addListener(map, 'drag', function () {
-	var message = '지도를 드래그 하고 있습니다. ' +
-					'지도의 중심 좌표는 ' + map.getCenter().toString() +' 입니다.';
-	console.log(message);
-});
-// 마커 이미지의 주소
-var markerImageUrl = 'http://t1.daumcdn.net/localimg/localimages/07/2012/img/marker_p.png',
-    markerImageSize = new daum.maps.Size(40, 42), // 마커 이미지의 크기
-    markerImageOptions = {
-        offset : new daum.maps.Point(20, 42)// 마커 좌표에 일치시킬 이미지 안의 좌표
-    };
-
-// 마커 이미지를 생성한다
-var markerImage = new daum.maps.MarkerImage(markerImageUrl, markerImageSize, markerImageOptions);
-
-// 지도에 마커를 생성하고 표시한다
-var marker = new daum.maps.Marker({
-    position: new daum.maps.LatLng(37.12345, 128.53068), // 마커의 좌표
-    draggable : true, // 마커를 드래그 가능하도록 설정한다
-    image : markerImage, // 마커의 이미지
-    map: map // 마커를 표시할 지도 객체
+  var message = '지도를 드래그 하고 있습니다. ' +
+          '지도의 중심 좌표는 ' + map.getCenter().toString() +' 입니다.';
+  console.log(message);
 });
 
-// 마커에 클릭 이벤트를 등록한다 (우클릭 : rightclick)
-daum.maps.event.addListener(marker, 'click', function() {
-    alert('마커를 클릭했습니다!');
-});
+var ps = new daum.maps.services.Places();
 
-// 마커에 mouseover 이벤트를 등록한다
-daum.maps.event.addListener(marker, 'mouseover', function() {
-    console.log('마커에 mouseover 이벤트가 발생했습니다!');
-});
+// 키워드로 장소를 검색합니다
+ps.keywordSearch('이태원 맛집', placesSearchCB);
 
-// 마커에 mouseout 이벤트 등록
-daum.maps.event.addListener(marker, 'mouseout', function() {
-    console.log('마커에 mouseout 이벤트가 발생했습니다!');
-});
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+    if (status === daum.maps.services.Status.OK) {
 
-// 마커에 dragstart 이벤트 등록
-daum.maps.event.addListener(marker, 'dragstart', function() {
-    console.log('마커에 dragstart 이벤트가 발생했습니다!');
-});
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new daum.maps.LatLngBounds();
 
-// 마커에 dragend 이벤트 등록
-daum.maps.event.addListener(marker, 'dragend', function() {
-    console.log('마커에 dragend 이벤트가 발생했습니다!');
-});
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);
+            bounds.extend(new daum.maps.LatLng(data[i].y, data[i].x));
+        }
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    }
+}
--- js/myfunction.js
+++ js/myfunction.js
@@ -2,3 +2,11 @@ var loadFile = function(event) {
   var output = document.getElementById('output');
   output.src = URL.createObjectURL(event.target.files[0]);
 };
+
+Var searchForm = document.querySelector('.search-form');
+var inputLocation = document.querySelector('.input-location');
+searchForm.addEventListener('submit', callMap);
+function callMap(e) {
+  e.preventDefault();
+  ps.keywordSearch(searchLoc.value, placesSearchCB);
+}
