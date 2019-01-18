e3de9886f4f04a7f1ce3bec226df97571765d083
Sunjin Lee <alpinist07@naver.com>
2019-01-18 17:16:52 +0900

    maker.html 페이지 업로드

--- js/myfunction.js
+++ js/myfunction.js
@@ -1,8 +1,10 @@
+// 사진 업로드
 var loadFile = function(event) {
   var output = document.getElementById('output');
   output.src = URL.createObjectURL(event.target.files[0]);
 };
 
+// 검색창에서 검색값 가져와 지도 호출하기
 var searchForm = document.querySelector('.search-form');
 var inputLocation = document.querySelector('.input-location');
 searchForm.addEventListener('submit', callMap);
